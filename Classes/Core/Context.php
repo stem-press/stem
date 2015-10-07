c2ad3b1a8da754d73f96ddf98982a1c536753d1f
Jon Gilkison <jon.gilkison@gmail.com>
2015-10-07 18:15:04 +0700

    Strip hashtags and force SSL on canonical urls

--- Classes/Core/Context.php
+++ Classes/Core/Context.php
@@ -642,8 +642,38 @@ class Context {
         wp_head();
         $header=ob_get_clean();
         $header=preg_replace("/<!--\\s*(?:.*)Yoast(?:.*)-->/", "", $header);
-        // TODO: Relative URL filtering
 
+        //fix yoast canonical URLs
+        if (isset($this->config['clean']['fix']['yoast']['force-ssl-canonical']) && $this->config['clean']['fix']['yoast']['force-ssl-canonical']) {
+            $header=str_replace('<link rel="canonical" href="http://','<link rel="canonical" href="https://',$header);
+        }
+
+        // strip hash tags from ngfb descriptions
+        if (isset($this->config['clean']['fix']['ngfb']['strip-hash-tags']) && $this->config['clean']['fix']['ngfb']['strip-hash-tags']) {
+            $matches=[];
+            if (preg_match_all("/<meta property=\"og:description\" content=\"(.*)?\"\\/>/", $header, $matches, PREG_OFFSET_CAPTURE)) {
+                $matchCount=count($matches[1]);
+                for($i=$matchCount; $i--; $i>-1) {
+                    $capture=$matches[1][$i];
+                    $origStr=$capture[0];
+                    $newStr=preg_replace("/(#[aA-zZ0-9-_]+)/", "", $origStr);
+                    $newStr=trim($newStr);
+                    $header=substr_replace($header,$newStr,$capture[1],strlen($origStr));
+                }
+            }
+
+            $matches=[];
+            if (preg_match_all("/<meta itemprop=\"description\" content=\"(.*)?\"\\/>/", $header, $matches, PREG_OFFSET_CAPTURE)) {
+                $matchCount=count($matches[1]);
+                for($i=$matchCount; $i--; $i>-1) {
+                    $capture=$matches[1][$i];
+                    $origStr=$capture[0];
+                    $newStr=preg_replace("/(#[aA-zZ0-9-_]+)/", "", $origStr);
+                    $newStr=trim($newStr);
+                    $header=substr_replace($header,$newStr,$capture[1],strlen($origStr));
+                }
+            }
+        }
 
         return $header;
     }
