5f78f99ffc1b7500467fc3f03d863058fd7f2e39
Jon Gilkison <jon.gilkison@gmail.com>
2018-11-13 04:31:31 +0700

    Bump version

--- stem.php
+++ stem.php
@@ -4,7 +4,7 @@ Plugin Name: ILAB Stem App Framework
 Plugin URI: https://github.com/jawngee/stem
 Description: Framework for building applications using Wordpress and Symfony
 Author: Jon Gilkison
-Version: 0.2.4
+Version: 0.4.0
 Author URI: http://interfacelab.com
 */
 
